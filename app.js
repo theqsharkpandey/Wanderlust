b254066c4095897636de35cd42992ccd606a6343
theqsharkpandey <kushagrapandey3000@gmail.com>
2025-12-28 20:00:58 +0530

    Fixed dbUrl undefined issue

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
+const dbUrl = process.env.MONGO_URL;
+
 main()
   .then(() => {
     console.log("connected to DB");
