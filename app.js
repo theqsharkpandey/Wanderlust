e28ef3ef0093b9e33aa1bd107cdffac97af4c246
theqsharkpandey <kushagrapandey3000@gmail.com>
2025-12-28 19:43:11 +0530

    Updated Project Files

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = process.env.MONGO_URL;
 
 main()
   .then(() => {
